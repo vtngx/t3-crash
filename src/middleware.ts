e78cfdce9fd3adcc848d5a060aef10061e3e666b
vtngx <vieth.nguyen2505@gmail.com>
2023-06-08 13:55:17 +0700

    feat: init clerk sso auth

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+    debug: true
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import { type AppType } from "next/app";
-import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { api } from "~/utils/api";
+import { type AppType } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const App: AppType = ({ Component, pageProps }) => {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(App);
