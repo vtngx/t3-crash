4a09bbfb5431616e7218f2d98bf4976247d7b6ef
vtngx <vieth.nguyen2505@gmail.com>
2023-06-08 15:13:50 +0700

    feat: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import "~/styles/globals.css";
 import { api } from "~/utils/api";
 import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 
 const App: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
+      <Analytics />
     </ClerkProvider>
   );
 };
